1fd4eb32d5cde6217b72393e8a58e55c0dcdf3b9
Prasanna V <prasannacse2005@gmail.com>
2019-11-15 13:39:41 +0530

    Add flags for sorting and weeks count

--- cmd/root.go
+++ cmd/root.go
@@ -11,9 +11,26 @@ var rootCmd = &cobra.Command{
   Short: "Get commit info for the Github Repository",
   Long: `Given a repository name, fetch the stats based on the commit history.
   Please check help for more detailed instructions.`,
-  Run: func(cmd *cobra.Command, args []string) {
-    fmt.Println("Hello World Cobra")
+  RunE: func(cmd *cobra.Command, args []string) error {
+    weeks, err := cmd.Flags().GetInt("weeks")
+    if err != nil {
+      return err
+    }
+    sort, err := cmd.Flags().GetString("sort")
+    if err != nil {
+      return err
+    }
+    fmt.Printf("Fetching Github Stats for %d and sorting by %s\n", weeks, sort)
+    return nil
   },
+}
+
+var weeks int
+var sort string
+
+func init() {
+  rootCmd.Flags().IntVarP(&weeks, "weeks", "w", 52, "No of weeks to process")
+  rootCmd.Flags().StringVarP(&sort, "sort", "s", "asc", "Sort the statistics [asc|desc]")
 }
 
 func Execute() {
